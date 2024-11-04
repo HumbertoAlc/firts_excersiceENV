c2336d0447efe026d687c1599b50f83aa9088f32
humberto <humalcgar@gmail.com>
2024-11-03 22:57:57 -0600

    add destructuring to the list

--- createList.py
+++ createList.py
@@ -2,5 +2,19 @@
 
 fisrtList = [1,2,3,4,5,6,7,8]
 
-for num in fisrtList:
-    print(type(num))
+# for num in fisrtList:
+#     print(type(num))
+
+num1,num2,num3,num4,num5,num6,num7,num8 = fisrtList
+
+print(f" desempaquete {num1}")
+print(f" desempaquete {num2}")
+print(f" desempaquete {num3}")
+print(f" desempaquete {num4}")
+print(f" desempaquete {num5}")
+print(f" desempaquete {num6}")
+print(f" desempaquete {num7}")
+print(f" desempaquete {num8}")
+
+
+
